e711d6b4cff19a78e392a8a515ee5318b320e155
mors-templar <raybaalb@gmail.com>
2025-09-03 16:29:07 +0500

    created a initial template for the wizard code

--- welcome.h
+++ welcome.h
@@ -3,10 +3,14 @@
 
 #include <QDialog>
 #include <QString>
+#include <QStackedWidget>
+#include <string>
 
+QT_BEGIN_NAMESPACE
 namespace Ui {
 class Welcome;
 }
+QT_END_NAMESPACE
 
 class Welcome : public QDialog
 {
@@ -16,18 +20,33 @@ public:
     explicit Welcome(QWidget *parent = nullptr);
     ~Welcome();
 
-signals:
-    void accepted(int difficulty, const QString& folderPath);
+private:
+    // pointer to the generated UI
+    Ui::Welcome *ui;
+
+    // wizard state
+    int currentPageIndex = 0;   // track stackedWidget index
+    QString selectedFolder;     // store chosen folder
+    QString difficulty;         // "Easy", "Normal", "Hard"
+
+    // helper methods you’ll define in .cpp
+    void goToNextPage();
+    void goToPreviousPage();
+    void updateSummaryPage();
+    void chooseFolder();
+    void setDifficulty();
 
 private slots:
+    // slots for button actions
     void on_nextButton_clicked();
     void on_backButton_clicked();
     void on_finishButton_clicked();
-
     void on_browseButton_clicked();
 
-private:
-    Ui::Welcome *ui;
+    // slots for difficulty selection
+    void on_easyRadioButton_clicked();
+    void on_normalRadioButton_clicked();
+    void on_hardRadioButton_clicked();
 };
 
 #endif // WELCOME_H
